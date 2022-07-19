a937f350eeb30092bb573ddb7e5941d3f9e252b2
Guilherme Mendes <gsamendes@gmail.com>
2022-07-18 21:02:28 -0300

    first update
    first commit after set up unity and VSC.

---
+++ estudos-na-unity/Assets/Scripts/Script01.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script01 : MonoBehaviour
+{
+    // Iniciado toda vez que o jogo/programa é iniciado.
+    void Start()
+    {
+        
+    }
+
+    // Atualizado na quantidade de frames que está configurado
+    void Update()
+    {
+        
+    }
+}
