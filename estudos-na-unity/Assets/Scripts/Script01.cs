916f0283bf61e59f892be46b044a74a82ed24f11
Guilherme Mendes <gsamendes@gmail.com>
2022-07-19 21:02:31 -0300

    update 19072022
    Studying c# variables

--- estudos-na-unity/Assets/Scripts/Script01.cs
+++ estudos-na-unity/Assets/Scripts/Script01.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class Script01 : MonoBehaviour
 {
+    //Estudo das variáveis
+    
+    //1) Tipos de variáveis:
+    
+    int ValorInteiro;
+    float ValorDecimal;
+    string Texto;
+
+    //2) Variáveis Publicas e Privadas:
+    //variáveis publicas
+    public int ValorInt = 10;
+    public float ValorDec = 10.5f;
+    public string TextoDeclarado = "Seu texto escrito aqui";
+
+    //variáveis privadas
+    //private int ValorInt = int ValorInteiro
+    //private float ValorDec = float ValorDecimal;
+    //private string TextoDeclarado = string Texto;
+
+    //Variaveis de Objetos da Unity
+    public GameObject gameobject;
+    public Transform transformUnity;
+    public AddComponentMenu novoComponente;
+    public Rigidbody2D rig;
+
     // Iniciado toda vez que o jogo/programa é iniciado.
     void Start()
     {
