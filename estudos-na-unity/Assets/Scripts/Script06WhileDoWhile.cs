53c68961b441e79487e0cb707172727fdc1509b7
Guilherme Mendes <gsamendes@gmail.com>
2022-07-26 22:18:45 -0300

    update 26072022
    - update studies about While and Do/While

--- estudos-na-unity/Assets/Scripts/Script06WhileDoWhile.cs
+++ estudos-na-unity/Assets/Scripts/Script06WhileDoWhile.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class Script06WhileDoWhile : MonoBehaviour
 {
+    int QuantItens = 5;
+    int Money = 100;
+
     // Start is called before the first frame update
     void Start()
     {
+        //while (QuantItens > 0)
+        //{
+        //    Debug.Log("Item coletado");
+        //    QuantItens--;
+        //}
+
+        do
+        {
+            Debug.Log("Dinheiro :" + Money);
+            Money += 50;
+        }
+        while (Money <= 1000);
         
     }
 
