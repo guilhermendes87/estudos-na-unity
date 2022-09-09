91fd861e0fc4b3f443fc77578c56a25157e1d639
Guilherme Mendes <gsamendes@gmail.com>
2022-09-08 22:59:54 -0300

    update 20220908
    - studies about properties {get}{set}

---
+++ estudos-na-unity/Assets/Scripts/Script18Properties2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script18Properties2 : MonoBehaviour
+{
+    void Start()
+    {
+        Script18Properties player1 = new Script18Properties();
+        player1.Life = 35;
+        Debug.Log(player1.Life);
+
+    }
+}
+
